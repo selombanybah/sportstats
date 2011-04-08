5f01509b9c86bab8da3caf2a8f0310ecaa9d3341
lnn1974@gmail.com <lnn1974@gmail.com@21019a89-3409-ad1e-a22f-ec4ccda24922>
2011-04-08 22:55:42 +0000

    gii + mysql

---
+++  sportstats --username lnn1974@gmail.com/protected/data/sportstats.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Апр 09 2011 г., 00:02
+-- Версия сервера: 5.5.8
+-- Версия PHP: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `sportstats`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+
+--
+-- Дамп данных таблицы `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `email`) VALUES
+(1, 'test1', 'pass1', 'test1@example.com'),
+(2, 'test2', 'pass2', 'test2@example.com'),
+(3, 'test3', 'pass3', 'test3@example.com'),
+(4, 'test4', 'pass4', 'test4@example.com'),
+(5, 'test5', 'pass5', 'test5@example.com'),
+(6, 'test6', 'pass6', 'test6@example.com'),
+(7, 'test7', 'pass7', 'test7@example.com'),
+(8, 'test8', 'pass8', 'test8@example.com'),
+(9, 'test9', 'pass9', 'test9@example.com'),
+(10, 'test10', 'pass10', 'test10@example.com'),
+(11, 'test11', 'pass11', 'test11@example.com'),
+(12, 'test12', 'pass12', 'test12@example.com'),
+(13, 'test13', 'pass13', 'test13@example.com'),
+(14, 'test14', 'pass14', 'test14@example.com'),
+(15, 'test15', 'pass15', 'test15@example.com'),
+(16, 'test16', 'pass16', 'test16@example.com'),
+(17, 'test17', 'pass17', 'test17@example.com'),
+(18, 'test18', 'pass18', 'test18@example.com'),
+(19, 'test19', 'pass19', 'test19@example.com'),
+(20, 'test20', 'pass20', 'test20@example.com'),
+(21, 'test21', 'pass21', 'test21@example.com');
